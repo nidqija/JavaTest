127ba23f432f9fc167d933a8ce8818f736dd69d1
Raziq Din <144256646+nidqija@users.noreply.github.com>
2025-11-28 01:58:33 +0800

    added action listener on all buttons

--- JavaTutorial/Exercise1.java
+++ JavaTutorial/Exercise1.java
@@ -20,7 +20,7 @@ public class Exercise1 extends JFrame {
        button1.addActionListener(new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e){
-           JOptionPane.showMessageDialog(frame, "A Button was clicked!" , "Message" , JOptionPane.INFORMATION_MESSAGE);
+           JOptionPane.showMessageDialog(frame, "A Button was pushed!" , "Message" , JOptionPane.INFORMATION_MESSAGE);
 
         }
        });
@@ -30,6 +30,14 @@ public class Exercise1 extends JFrame {
        button2.setBounds(140 , 20 , 100 , 20);
        panel.add(button2);
 
+
+       button2.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(frame, "B Button was clicked!" , "message" , JOptionPane.INFORMATION_MESSAGE);
+         }
+       });
+
        JButton button3 = new JButton("Nothing");
        button3.setBounds(260 , 20 , 100 , 20);
        panel.add(button3);
@@ -38,17 +46,48 @@ public class Exercise1 extends JFrame {
        button4.setBounds(20 , 80 , 100 , 20);
        panel.add(button4);
 
+
+       button4.addActionListener(new ActionListener(){
+         @Override
+         public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(frame, "Button 0 was clicked!" , "message" , JOptionPane.INFORMATION_MESSAGE);
+         }
+       });
+
        JButton button5 = new JButton("Button 1");
        button5.setBounds(140 , 80 , 100 , 20);
        panel.add(button5);
+
+        button5.addActionListener(new ActionListener(){
+         @Override
+         public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(frame, "Button 1 was clicked!" , "message" , JOptionPane.INFORMATION_MESSAGE);
+         }
+       });
+
+       
 
        JButton button6 = new JButton("Button 2");
        button6.setBounds(260 , 80 , 100 , 20);
        panel.add(button6);
 
+         button6.addActionListener(new ActionListener(){
+         @Override
+         public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(frame, "Button 2 was clicked!" , "message" , JOptionPane.INFORMATION_MESSAGE);
+         }
+       });
+
        JButton button7 = new JButton("Button 3");
        button7.setBounds(140 , 140 , 100 , 20);
        panel.add(button7);
+
+         button7.addActionListener(new ActionListener(){
+         @Override
+         public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(frame, "Button 3 was clicked!" , "message" , JOptionPane.INFORMATION_MESSAGE);
+         }
+       });
 
 
 
